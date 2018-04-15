4c7276dd1ad90f5c1e641bc5cbb5c25a5669d5fb
catherinecompsci30 <36053924+catherinecompsci30@users.noreply.github.com>
2018-04-14 18:02:51 -0600

    Apr. 14 Update

--- Grid Based Game/start.js
+++ Grid Based Game/start.js
@@ -3,53 +3,72 @@
 // April 16, 2018
 
 //Memory game using elements from the periodic table
-// - Hydrogen (H)
-// - Boron (B)
-// - Carbon (C)
-// - Oxygen (O)
-// - Sodium (Na)
-// - Aluminum (Al)
-// - Chlorine (Cl)
-// - Potassium (K)
-// - Titanium (Ti)
-// - Iron (Fe)
-// - Copper (Cu)
-// - Bromine (Br)
-// - Yttrium (T)
-// - Tin (Sn)
-// - Antimony (Sb)
-// - Lead (Pb)
-// - Mercury (Hg)
-// - Einsteinium (Es)
 
 
+let state;
 let rows = 4;
 let cols = 4;
 let grid;
 let cellSize;
+let makeGuess = true;
+
+let allElements = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+let guessesMade = 0;
+let guess = [];
+
+let timeLeft;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellSize = (height - 200) / rows;
+
+  grid = elementLocations();
+
+  timeLeft = 5;
+
+  state = 1;
 }
 
 function draw() {
-  background(255);
-  displayGrid();
-  gameInstructions();
-  // startButton();
-  setUpGame();
-  elementLocations();
+  if (state === 1) {
+    background(255);
 
+    displayGrid();
+    gameInstructions();
+
+    userGuess();
+    countdownTimer();
+  }
+  else if (state === 2) {
+    resetGame();
+
+  }
 }
 
 
 function displayGrid() {
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) {
       fill(255);
       strokeWeight(5);
+      if (grid[x][y][2] === 0) {
+        fill(102, 204, 255);
+      }
+      else if (grid[x][y][2] === 1) {
+        fill(0, 255, 0);
+      }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
+
+      let textToDisplay = "";
+
+      if (grid[x][y][1] === 1) {
+        textToDisplay = displayElement(grid[x][y][0]);
+        fill(0);
+        textSize(30);
+        text(textToDisplay, x * cellSize + (cellSize / 2), y * cellSize + (cellSize / 2) + 10);
+      }
     }
   }
 }
@@ -61,71 +80,198 @@ function gameInstructions() {
   textSize(90);
   textAlign(CENTER);
   fill(63, 51, 255);
-  text("Memory Match", 1050, 200);
+  text("Memory Match", (width / 2) + 200, 100);
 
   let instructions = [""
     ,"Click on a box to reveal a chemical element. \n"
     ,"Find two of the same element in order to have a match.  \n"
+    ,"Press the SPACE BAR to make a new guess.  \n"
     ,"Find all matches before the time runs out!"
   ].join("");
 
   textSize(28);
   fill(0, 102, 153);
-  text(instructions, 1050, 300);
+  text(instructions, (width / 2) + 200, 180);
 }
 
-
-// function startButton() {
-//   let buttonWidth = 200;
-//   let buttonHeight = 100;
-//   let leftSide = 300;
-//   let topSide = 400;
-//   let rightSide = leftSide + buttonWidth;
-//   let bottomSide = topSide + buttonHeight;
-//
-//   fill(128, 120, 241);
-//   if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
-//     fill(179, 102, 255);
-//     if (mouseIsPressed) {
-//       setUpElements();
-//     }
-//   }
-//
-//   noStroke();
-//   rect(leftSide, topSide, buttonWidth, buttonHeight);
-//
-//   textStyle(BOLD);
-//   textSize(26);
-//   textAlign(CENTER, CENTER);
-//   fill(0);
-//   text("Start Game", leftSide + 0.5 * buttonWidth, topSide + 0.5 * buttonHeight);
-// }
-
-
-function setUpGame() {
-  for (let x=0; x<cols; x++) {
-    for (let y=0; y<rows; y++) {
-      strokeWeight(5);
-      fill(102, 204, 255);
-      rect(cellSize * x, cellSize * y, cellSize, cellSize);
-    }
-  }
-}
 
 //sets up where each element will be at the start of the game
 function elementLocations() {
   let choice;
-  let allElements = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
   let transferArray = [];
   let newShuffle = [];
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       choice = floor(random(allElements.length - 1));
-      transferArray.push(allElements[choice]);
+      transferArray.push([allElements[choice], 0, 0]);
       allElements.splice(choice, 1);
     }
     newShuffle.push(transferArray);
     transferArray = [];
   }
   return newShuffle;
+}
+
+
+function displayElement(element) {
+  let newString = "";
+
+  if (element === 1) {
+    newString = "H";
+  }
+
+  else if (element === 2) {
+    newString = "C";
+  }
+
+  else if (element === 3) {
+    newString = "O";
+  }
+
+  else if (element === 4) {
+    newString = "Cl";
+  }
+
+  else if (element === 5) {
+    newString = "Fe";
+  }
+
+  else if (element === 6) {
+    newString = "Hg";
+  }
+
+  else if (element === 7) {
+    newString = "Cu";
+  }
+
+  else if (element === 8) {
+    newString = "Pb";
+  }
+
+  return newString;
+}
+
+
+function userGuess() {
+  if (makeGuess && guessesMade < 2) {
+    let xPos = floor(mouseX / cellSize);
+    let yPos = floor(mouseY / cellSize);
+
+    if (mouseIsPressed) {
+      if (mouseX < cellSize * rows && mouseY < cellSize * cols) {
+        grid[xPos][yPos][1] = 1;
+        guessesMade += 1;
+        mouseIsPressed = false;
+
+        guess.push([grid[xPos][yPos][0], xPos, yPos]);
+      }
+    }
+  }
+
+  if (guessesMade === 2) {
+    compareUserGuess();
+    if (compareUserGuess()) {
+      grid[guess[0][1]][guess[0][2]][2] = 1;
+      grid[guess[1][1]][guess[1][2]][2] = 1;
+    }
+
+  }
+}
+
+
+function compareUserGuess() {
+  textSize(56);
+  if (guess[0][0] === guess[1][0]) {
+    fill(0, 255, 0);
+    text("It's a match!", (cellSize * rows) / 2, (height / 5) * 4);
+    return true;
+  }
+  else {
+    fill(255, 0, 0);
+    text("Nope!", (cellSize * rows) / 2, (height / 5) * 4);
+    return false;
+  }
+}
+
+
+function keyTyped() {
+  if (state === 1) {
+    if (key === " ") {
+      guess = [];
+      guessesMade = 0;
+
+      for (let x=0; x<cols; x++) {
+        for (let y=0; y<rows; y++) {
+          grid[x][y][1] = 0;
+        }
+      }
+    }
+  }
+  else if (state === 2) {
+    if (key === " ") {
+      guess = [];
+      guessesMade = 0;
+      state = 1;
+    }
+  }
+}
+
+
+function countdownTimer() {
+  if (timeLeft >= 0) {
+    if (frameCount % 60 !== 0) {
+      textSize(20);
+      fill(0);
+      textAlign(CENTER);
+      text('Time Remaining: ' + timeLeft + ' seconds', width / 2 + 200 , 380);
+    }
+    else {
+      timeLeft -= 1;
+    }
+  }
+  else {
+    state = 2;
+  }
+}
+//   startTime = 3;
+//   timePassed = Math.round(floor(millis() / 1000));
+//   timeLeft = startTime - timePassed;
+//
+//   if (state === 1) {
+//     fill(0);
+//     for (let seconds = startTime; seconds > 0; seconds--) {
+//       textSize(20);
+//       textAlign(CENTER);
+//       text('Time Remaining: ' + timeLeft + ' seconds', width / 2 + 200 , 380);
+//     }
+//     if (timeLeft === 0) {
+//       state = 2;
+//     }
+//   }
+// }
+
+
+function resetGame() {
+  background(0);
+  textStyle(BOLD);
+  textSize(200);
+  textAlign(CENTER);
+  fill(255, 0, 0);
+  text("GAME OVER", width/2, height/2);
+
+  textSize(30);
+  text("Press SPACE BAR to restart.", width/2, height/2 + 100);
+
+  guess = [];
+  guessesMade = 0;
+
+  for (let x=0; x<cols; x++) {
+    for (let y=0; y<rows; y++) {
+      grid[x][y][1] = 0;
+    }
+  }
+
+  elementLocations();
+
+  timeLeft = 5;
 }
--- Traffic Light Demo/sketch.js
+++ Traffic Light Demo/sketch.js
@@ -6,84 +6,109 @@
 // changing according to time. You may want to investigate the millis()
 // function at processing.org/reference.
 
-let state;
-let redLightDuration, yellowLightDuration, greenLightDuration;
-let lastTimeLightChanged;
+
+
+let timeLeft = 12;
+let running;
+let lastMillis;
 
 function setup() {
-  createCanvas(600, 600);
-  state = 1;
-  redLightDuration = 3000;
-  yellowLightDuration = 500;
-  greenLightDuration = 3000;
-  lastTimeLightChanged = millis();
+  createCanvas(400, 400);
+  lastMillis = millis() / 1000;
+  running = true;
 }
-
 
 function draw() {
   background(255);
-  drawOutlineOfLights();
-  checkIfLightSwitched();
-  displayCorrectLight();
-}
-
-function checkIfLightSwitched () {
-  if (state === 1) { //green
-    if (millis() > lastTimeLightChanged + greenLightDuration) {
-      state = 2;
-      lastTimeLightChanged = millis();
-    }
-  }
-  if (state ===2) { //yellow
-    if (millis() > lastTimeLightChanged + yellowLightDuration) {
-      state = 3;
-      lastTimeLightChanged = millis();
-    }
-  }
-  else if (state === 3) { //red
-    if (millis() > lastTimeLightChanged + redLightDuration) {
-      state = 1;
-      lastTimeLightChanged = millis();
-    }
-  }
-}
-
-function displayCorrectLight() {
-  if (state ===1) {
-    drawGreenLight();
-  }
-  else if (state === 2) {
-    drawYellowLight();
-  }
-  else if (state === 3) {
-    drawRedLight();
-  }
-}
-
-function drawGreenLight() {
-  fill(0, 255, 0);
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
-}
-
-function drawYellowLight() {
-  fill(255, 255, 0);
-  ellipse(width/2, height/2, 50, 50); //middle
-}
-
-function drawRedLight () {
-  fill(255, 0, 0);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
-}
-
-function drawOutlineOfLights() {
-  //box
-  rectMode(CENTER);
   fill(0);
-  rect(width/2, height/2, 75, 200, 10);
-
-  //lights
-  fill(255);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
-  ellipse(width/2, height/2, 50, 50); //middle
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  if (running) {
+    timeLeft -= ((millis() / 1000) - lastMillis);
+  }
+  lastMillis = millis() / 1000;
+  text(timeLeft, 20, 60);
+  if(timeLeft < 0 ){
+    timeLeft = int(12);
+  }
 }
+
+// let state;
+// let redLightDuration, yellowLightDuration, greenLightDuration;
+// let lastTimeLightChanged;
+//
+// function setup() {
+//   createCanvas(600, 600);
+//   state = 1;
+//   redLightDuration = 3000;
+//   yellowLightDuration = 500;
+//   greenLightDuration = 3000;
+//   lastTimeLightChanged = millis();
+// }
+//
+//
+// function draw() {
+//   background(255);
+//   drawOutlineOfLights();
+//   checkIfLightSwitched();
+//   displayCorrectLight();
+// }
+//
+// function checkIfLightSwitched () {
+//   if (state === 1) { //green
+//     if (millis() > lastTimeLightChanged + greenLightDuration) {
+//       state = 2;
+//       lastTimeLightChanged = millis();
+//     }
+//   }
+//   if (state ===2) { //yellow
+//     if (millis() > lastTimeLightChanged + yellowLightDuration) {
+//       state = 3;
+//       lastTimeLightChanged = millis();
+//     }
+//   }
+//   else if (state === 3) { //red
+//     if (millis() > lastTimeLightChanged + redLightDuration) {
+//       state = 1;
+//       lastTimeLightChanged = millis();
+//     }
+//   }
+// }
+//
+// function displayCorrectLight() {
+//   if (state ===1) {
+//     drawGreenLight();
+//   }
+//   else if (state === 2) {
+//     drawYellowLight();
+//   }
+//   else if (state === 3) {
+//     drawRedLight();
+//   }
+// }
+//
+// function drawGreenLight() {
+//   fill(0, 255, 0);
+//   ellipse(width/2, height/2 + 65, 50, 50); //bottom
+// }
+//
+// function drawYellowLight() {
+//   fill(255, 255, 0);
+//   ellipse(width/2, height/2, 50, 50); //middle
+// }
+//
+// function drawRedLight () {
+//   fill(255, 0, 0);
+//   ellipse(width/2, height/2 - 65, 50, 50); //top
+// }
+//
+// function drawOutlineOfLights() {
+//   //box
+//   rectMode(CENTER);
+//   fill(0);
+//   rect(width/2, height/2, 75, 200, 10);
+//
+//   //lights
+//   fill(255);
+//   ellipse(width/2, height/2 - 65, 50, 50); //top
+//   ellipse(width/2, height/2, 50, 50); //middle
+//   ellipse(width/2, height/2 + 65, 50, 50); //bottom
+// }
