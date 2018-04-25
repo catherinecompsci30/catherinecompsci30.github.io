a600c600d37c0d42750b0378e2c7ea1ef761817b
Liu1301430 <1301430@sps-student.sk.ca>
2018-04-25 15:19:35 -0600

    Apr. 25 Update

--- Pair Programming Terrain/sketch.js
+++ Pair Programming Terrain/sketch.js
@@ -7,25 +7,40 @@ let stretch;
 let angle;
 let inc = (Math.PI)*2 / 80.0;
 let heightsArray = [];
+let transferArray = [];
 
 function setup() {
   createCanvas (1200, 600);
   amp = 40;
-  stretch = 4;
+
+  generateWaveHeights(5);
+
 }
 
 function draw() {
   background(255);
-  oneTerrainChunk();
+  strokeWeight(3);
+  drawSinWave();
 }
 
-function oneTerrainChunk() {
-  strokeWeight(1);
-  smooth();
+function generateWaveHeights(stretch) {
   angle = 0;
   for (let i = 0; i < 80; i++) {
-    line(i * stretch, height, i *  stretch, height/1.3 - 2*(sin(angle) * amp));
     heightsArray.push([i *  stretch, height/1.3 - 2*(sin(angle) * amp)]);
     angle += inc;
+  }
+  return heightsArray;
+}
+
+
+function drawSinWave() {
+  for (let i = 0; i < heightsArray.length; i++) {
+    if (i === heightsArray.length - 1) {
+      point(heightsArray[i][0], heightsArray[i][1]);
+    }
+    else {
+      line(heightsArray[i][0], heightsArray[i][1],heightsArray[i+1][0], heightsArray[i+1][1] );
+    }
+    // heightsArray[i][0] += 1;
   }
 }
