0d38cbb4eca4a59de550ce00499c506368105b72
Liu1301430 <1301430@sps-student.sk.ca>
2018-05-01 14:43:57 -0600

    Final Update

--- Pair Programming Project/sketch.js
+++ Pair Programming Project/sketch.js
@@ -98,13 +98,14 @@ function draw() {
     // - air resistance, friction, etc.
     if (mouseIsPressed) {
       moveLength += 0.1;
+      myBall.yVelocity += 0.5;
       if (moveLength >= 22) {
         moveLength = 22;
       }
     }
     else {
       if (moveLength > 6 && moveLength < 0) {
-        moveLength -= 0.07;
+        moveLength -= 0.09;
       }
     }
   }
@@ -241,81 +242,5 @@ function drawSinWave() {
       stretch = 5;
     }
     generateWaveHeights(stretch, 1200);
-  }
-}
-
-
-
-class Ball {
-  constructor() {
-
-    this.x = 200;
-    this.y = 20;
-    this.prevY = this.y;
-    this.radius = 2;
-
-    this.yVelocity = 0;
-    this.gravity = 0.2;
-
-    this.lowerRange;
-
-    this.contact = false;
-    this.hit = false;
-  }
-
-  //displays ball.
-  display() {
-    fill(255);
-    ellipse(this.x, this.y, this.radius, this.radius);
-  }
-
-  //completes all calculations neccesary for the motion of the ball.
-  move() {
-    //uses the map() function to variate radius and gravitational strength based
-    //on the height of the ball.
-    this.radius = map(this.y, 0, windowHeight, 5, 70);
-    this.gravity = map(this.y, 0, windowHeight, 0.2, 1);
-
-    //applies gravitational force to ball.
-    this.yVelocity += this.gravity;
-    this.y += this.yVelocity;
-
-    //controls ball behaviour at boundary of sin waves to remain on terrain and
-    //calculates ball velocity based on slope of each 'hill'.
-    if (this.y >= this.lowerRange) {
-      //locks position to hill.
-      this.y = this.lowerRange;
-
-      //if this is the first contact...
-      if (!this.contact) {
-        //if the ball makes contact with a section of a hill with positive slope
-        //and is travelling above a certain x velocity, the ball will lose momentum,
-        //and you could potentially die.
-        if (heightsArray[this.index][1] > heightsArray[this.index + 1][1] + 4  && this.yVelocity > 20) {
-          if (moveLength >= 15) {
-            state = 3;
-          }
-          this.hit = true;
-          // moveLength -= 1;
-        }
-        this.contact = true;
-      }
-      else {
-        this.contact = false;
-      }
-    }
-
-    //calcualtes current y velocity by looking at the change in pixels in one frame.
-    this.yVelocity = this.y - this.prevY;
-    //sets limitation on maximum velocity.
-    if (this.yVelocity < -16) {
-      this.yVelocity = -16;
-    }
-    if (this.hit) {
-      this.yVelocity = 60;
-      this.hit = false;
-    }
-    //holds information about the previous position of the ball.
-    this.prevY = this.y;
   }
 }
